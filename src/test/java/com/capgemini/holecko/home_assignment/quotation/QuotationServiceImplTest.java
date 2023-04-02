e7ea1e480dd9204f91065b378b3678715e9dae2f
xholecko <stevoh096@gmail.com>
2023-04-02 22:16:26 +0200

    finish removing boilerplate code

--- src/test/java/com/capgemini/holecko/home_assignment/quotation/QuotationServiceImplTest.java
+++ src/test/java/com/capgemini/holecko/home_assignment/quotation/QuotationServiceImplTest.java
@@ -4,18 +4,15 @@ import com.capgemini.holecko.home_assignment.customer.Customer;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureTestDatabase
 @Transactional
